638f8b5615a7b908c0dd3e1968a3fccf1143091f
= <=>
2023-11-26 00:00:46 +0100

    :rocket: add of gamejoin

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 #define GAME_LIST_PATH "json/gameslist.json"
 #define MAPS_LIST_PATH "json/mapslist.json"
 #define GAME_CREATE_PATH "json/gamecreate.json"
+#define GAME_JOIN_PATH "json/gamejoin.json"
 
 int port = 42069;
 int num_clients = 0;
@@ -51,11 +52,11 @@ int setup_server_socket(Server *server_manager, int port, int type)
 }
 
 /** @brief Reads a json file and sends it's content to the client file descriptor */
-int read_json_file(int *cFd, char *path_json_file, char **content)
+int read_json_file(char *path_json_file, char **content)
 {
-    FILE *games_list_file = fopen(path_json_file, "r++");
+    FILE *games_list_file = fopen(path_json_file, "r");
     if (!games_list_file)
-        handle_error("fopen games_list_file \"r++\"", -1);
+        handle_error("fopen games_list_file \"r\"", -1);
 
     fseek(games_list_file, 0, SEEK_END);
     long tell = ftell(games_list_file);
@@ -72,7 +73,8 @@ int read_json_file(int *cFd, char *path_json_file, char **content)
 }
 
 /** @brief Function to update the game file when a client connects to the game he chose. */
-int action_game_join(int *cFd, char *buffer)
+//XXX: récupérer la bonne mapId dans gamelist pour mettre a jour gamejoin
+int action_game_join(char *buffer)
 {
     char buff[strlen(buffer) - 15];
     for (int i = 0; i < strlen(buffer) - 15; i++)
@@ -83,76 +85,24 @@ int action_game_join(int *cFd, char *buffer)
     cJSON *file_join_game = cJSON_Parse(buff);
     if (file_join_game == NULL)
     {
-        handle_json_error("new_game");
+        handle_json_error("file_join_game");
         cJSON_Delete(file_join_game);
         return -1;
     }
 
     cJSON *game_name = cJSON_GetObjectItemCaseSensitive(file_join_game, "name");
-    if (!(cJSON_IsString(game_name)) || (game_name->valuestring == NULL))
-        handle_error("game_name: bad usage", -1);
 
-    const char *join_game_file = "json/";
-    size_t len_path = strlen(join_game_file) + strlen(game_name->valuestring) + 1;
-    char join_game_path[len_path];
-    sprintf(join_game_path, "%s%s.json", join_game_file, game_name->valuestring);
-
-    FILE *game = fopen(join_game_path, "r");
-    if (game == NULL)
-        handle_error("attempt to join game failed", -1);
-
-    fseek(game, 0, SEEK_END);
-    long tell = ftell(game);
-    fseek(game, 0, SEEK_SET);
-
-    char *content = (char *)calloc(tell, sizeof(char));
-    if (fread(content, 1, tell, game) < 0)
-        handle_error("action_game_create(): fread", -1);
-
-    fclose(game);
-
-    cJSON *root = cJSON_Parse(content);
-    if (root == NULL)
-    {
-        handle_json_error("root");
-        cJSON_Delete(root);
-        return -1;
-    }
-
-    cJSON *nbPlayers = cJSON_GetObjectItemCaseSensitive(root, "nbPlayers");
-    cJSON_ReplaceItemInObjectCaseSensitive(root, "nbPlayers", cJSON_CreateNumber(nbPlayers->valueint + 1));
-
-    cJSON *array_players = cJSON_GetObjectItemCaseSensitive(root, "players");
-    cJSON *join_player_data = cJSON_CreateObject();
-    cJSON_AddNumberToObject(join_player_data, "id", *cFd);
-    cJSON *join_player_pos = cJSON_GetObjectItemCaseSensitive(root, "startPos");
-    cJSON_AddStringToObject(join_player_data, "pos", join_player_pos->valuestring);
-    cJSON_ReplaceItemInObjectCaseSensitive(root, "startPos", cJSON_CreateString("3,2"));
-    cJSON_AddItemToArray(array_players, join_player_data);
-
-    char *json_str = cJSON_Print(root);
-    game = fopen(join_game_path, "w");
-    if (game == NULL)
-        handle_error("fopen game_create_json \"w\"", -1);
-
-    printf("%s\n", json_str);
-    fputs(json_str, game);
-    fclose(game);
-
-    FILE *games_list = fopen(GAME_LIST_PATH, "w");
-    fseek(games_list, 0, SEEK_END);
-    tell = ftell(games_list);
-    fseek(games_list, 0, SEEK_SET);
-
-    content = (char *)calloc(tell, sizeof(char));
-    if (fread(content, 1, tell, games_list) < 0)
-        handle_error("action_game_create(): fread", -1);
-
-    fclose(games_list);
+    char *content = (char *)calloc(MAX_SIZE_MESSAGE, sizeof(char));
+    read_json_file(GAME_LIST_PATH, &content);
 
     cJSON *cj_games_list = cJSON_Parse(content);
-    if (cj_games_list != NULL)
+    if (cj_games_list == NULL)
     {
+        handle_json_error("cj_games_list");
+        cJSON_Delete(cj_games_list);
+        free(content);
+        return -1;
+    } else if (cj_games_list != NULL) {
         cJSON *games = cJSON_GetObjectItemCaseSensitive(cj_games_list, "games");
         if (games != NULL)
         {
@@ -160,115 +110,40 @@ int action_game_join(int *cFd, char *buffer)
             cJSON_ArrayForEach(element, games)
             {
                 cJSON *element_name = cJSON_GetObjectItemCaseSensitive(element, "name");
-                printf("element_name: %s\n", element_name->valuestring);
                 if (strcmp(element_name->valuestring, game_name->valuestring) == 0)
                 {
                     cJSON *element_nb_player = cJSON_GetObjectItemCaseSensitive(element, "nbPlayers");
-                    cJSON_ReplaceItemInObjectCaseSensitive(element_nb_player, "nbPlayers", cJSON_CreateNumber(element_nb_player->valueint + 1));
+                    cJSON_ReplaceItemInObjectCaseSensitive(element, "nbPlayers", cJSON_CreateNumber(element_nb_player->valueint + 1));
                     char *json_print = cJSON_Print(cj_games_list);
                     FILE *games_list = fopen(GAME_LIST_PATH, "w");
-                    if (games_list != NULL)
-                        fputs(json_print, games_list);
+                    if (games_list == NULL)
+                         handle_error("fopen game_list \"w\"", -1);
+
+                    printf("%s\n", json_print);
+                    fputs(json_print, games_list);
                     fclose(games_list);
                     break;
                 }
             }
         }
-        cJSON_Delete(games);
     }
 
     cJSON_Delete(cj_games_list);
-    cJSON_Delete(root);
+    free(content);
     cJSON_Delete(file_join_game);
 
     return 0;
 }
 
-/** @brief Function to create the game file when client uses POST game/create. */
-int create_running_game_data(char *game_name, int *cFd)
-{
-    const char *new_game_file = "json/";
-    size_t len_path = strlen(new_game_file) + strlen(game_name) + 1;
-    char new_game_path[len_path];
-    sprintf(new_game_path, "%s%s.json", new_game_file, game_name);
-
-    FILE *game = fopen(new_game_path, "w");
-    if (game == NULL)
-        handle_error("temp game file fopen", -1);
-
-    fclose(game);
-
-    cJSON *new_game = cJSON_CreateObject();
-    cJSON_AddStringToObject(new_game, "action", "game/join");
-    cJSON_AddStringToObject(new_game, "statut", "201");
-    cJSON_AddStringToObject(new_game, "message", "game joined");
-    cJSON_AddNumberToObject(new_game, "nbPlayers", 1);
-    cJSON *players = cJSON_CreateArray();
-    cJSON *player_info = cJSON_CreateObject();
-    cJSON_AddNumberToObject(player_info, "id", *cFd);
-    cJSON_AddStringToObject(player_info, "pos", "5,3");
-    cJSON_AddItemToArray(players, player_info);
-    cJSON_AddItemToObject(new_game, "players", players);
-    cJSON_AddStringToObject(new_game, "startPos", "5,3");
-    cJSON *player = cJSON_CreateObject();
-    cJSON_AddNumberToObject(player, "life", 100);
-    cJSON_AddNumberToObject(player, "speed", 1);
-    cJSON_AddNumberToObject(player, "nbClassicBomb", 1);
-    cJSON_AddNumberToObject(player, "nbMine", 0);
-    cJSON_AddNumberToObject(player, "nbRemoteBomb", 0);
-    cJSON_AddNumberToObject(player, "impactDist", 2);
-    cJSON_AddBoolToObject(player, "invincible", cJSON_False);
-    cJSON_AddItemToObject(new_game, "player", player);
-
-    char *json_print = cJSON_Print(new_game);
-
-    game = fopen(new_game_path, "w");
-    if (game == NULL)
-        handle_error("temp game file fopen", -1);
-
-    fputs(json_print, game);
-    fclose(game);
-    return 0;
-}
-
-/** @brief This function is used to manage a running game with a client and the json file of the game.
- *  @note This function is yet to be done.
- */
-int running_game(int *cFd, char *game_name)
-{
-    const char *new_game_file = "json/";
-    size_t len_path = strlen(new_game_file) + strlen(game_name) + 1;
-    char game_path[len_path];
-    sprintf(game_path, "%s%s.json", new_game_file, game_name);
-
-    char *response = (char *)calloc(256, sizeof(char));
-    if (read_json_file(cFd, game_path, &response) < 0)
-        handle_error("running_game(): read_json_file()", -1);
-
-    if (response != NULL)
-        send(*cFd, response, strlen(response), 0);
-
-    return 0;
-}
 
 /** @brief Function called when client send `POST game/create` request.
+ *  modify the gamelist.json
  *  @return -1 when in error case (using the `handle_error` MACRO). 0 when no errors
  */
-int action_game_create(int *cFd, char *buffer)
+int action_game_create(char *buffer)
 {
-    FILE *game_create_json = fopen(GAME_LIST_PATH, "r");
-    if (game_create_json == NULL)
-        handle_error("fopen game_create_json \"r\"", -1);
-
-    fseek(game_create_json, 0, SEEK_END);
-    long tell = ftell(game_create_json);
-    fseek(game_create_json, 0, SEEK_SET);
-
-    char *content = (char *)calloc(tell, sizeof(char));
-    if (fread(content, 1, tell, game_create_json) < 0)
-        handle_error("action_game_create(): fread", -1);
-
-    fclose(game_create_json);
+    char *content = (char *)calloc(MAX_SIZE_MESSAGE, sizeof(char));
+    read_json_file(GAME_LIST_PATH, &content);
 
     // Parse the actual gamesList json file
     cJSON *root = cJSON_Parse(content);
@@ -309,11 +184,8 @@ int action_game_create(int *cFd, char *buffer)
         cJSON_AddNumberToObject(new_game_data, "mapId", new_game_map->valueint);
     cJSON_AddItemToArray(array_games, new_game_data);
 
-    if (create_running_game_data(new_game_name->valuestring, cFd) < 0)
-        handle_error("action_game_create(): create_running_game_data()", -1);
-
     char *json_str = cJSON_Print(root);
-    game_create_json = fopen(GAME_LIST_PATH, "w");
+    FILE *game_create_json = fopen(GAME_LIST_PATH, "w");
 
     if (game_create_json == NULL)
         handle_error("fopen game_create_json \"w\"", -1);
@@ -323,7 +195,6 @@ int action_game_create(int *cFd, char *buffer)
     fclose(game_create_json);
 
     cJSON_Delete(root);
-    running_game(cFd, new_game_name->valuestring);
     cJSON_Delete(new_game);
 
     return 0;
@@ -347,20 +218,24 @@ void *answer_server(void *arg)
         printf("User %d: %s\n", id, buffer);
         if (strncmp(buffer, "POST game/create", 16) == 0)
         {
-            action_game_create(&client_socket, buffer);
+            action_game_create(buffer);
+            if (read_json_file(GAME_CREATE_PATH, &response) < 0)
+                handle_error_noexit("GET game/create");
         }
         else if (strncmp(buffer, "POST game/join", 14) == 0)
         {
-            action_game_join(&client_socket, buffer);
+            action_game_join(buffer);
+            if (read_json_file(GAME_JOIN_PATH, &response) < 0)
+                handle_error_noexit("GET game/join");
         }
         else if (strncmp(buffer, "GET maps/list", 13) == 0)
         {
-            if (read_json_file(&client_socket, MAPS_LIST_PATH, &response) < 0)
+            if (read_json_file(MAPS_LIST_PATH, &response) < 0)
                 handle_error_noexit("GET maps/list");
         }
         else if (strncmp(buffer, "GET game/list", 13) == 0)
         {
-            if (read_json_file(&client_socket, GAME_LIST_PATH, &response) < 0)
+            if (read_json_file(GAME_LIST_PATH, &response) < 0)
                 handle_error_noexit("GET game/list");
         }
         else if (strncmp(buffer, "looking for bomberstudent servers", 33) == 0)
